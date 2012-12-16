b277c259da18eca179a342459e9c4901db1acb59
josego85@gmail.com <josego85@gmail.com@2addb48d-d336-3729-c4b7-466610f16392>
2012-12-16 10:32:22 +0000

    pedidos

--- trunk/application/views/output.php
+++ trunk/application/views/output.php
@@ -3,4 +3,9 @@
  * Esta vista se encargara de devolver los valores solicitados a las aplicaciones clientes. Ej json, xml
  * @author Juan Bauer
  */
-echo json_encode($p_output);
+/*echo json_encode($p_output);*/
+$RSS_PHP = new rss_php;
+
+$RSS_PHP->loadArray($p_output); #the second param is an optional root node name.
+
+echo $RSS_PHP->getXML();

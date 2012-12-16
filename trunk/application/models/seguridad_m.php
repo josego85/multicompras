6ecd0b8f90d12b03262f09ddda4c3923302485b3
josego85@gmail.com <josego85@gmail.com@2addb48d-d336-3729-c4b7-466610f16392>
2012-12-16 00:06:57 +0000

    seguridad y login

--- trunk/application/models/seguridad_m.php
+++ trunk/application/models/seguridad_m.php
@@ -2,7 +2,7 @@
 
 class Seguridad_M extends CI_Model {
 	/**
-	 * Modelo para manejo de seguridad para 4s.
+	 * Modelo para manejo de seguridad para mc.
 	 * @author Juan Bauer
 	 */
 	public function __construct(){
@@ -15,18 +15,17 @@ class Seguridad_M extends CI_Model {
 	 * @param String password de acceso
 	 * @return boolean
 	 */
-	public function login($p_user,$p_password)
+	public function login($p_email,$p_password)
 	{
-		$this->db->select('usuario_user, usuario_id');
-		$this->db->where('usuario_user',$p_user);
-		$this->db->where('usuario_pwd',$p_password);
-		$this->db->where('usuario_estado',E_ACTIVO);
+		$this->db->select('cliente_email, cliente_id');
+		$this->db->where('cliente_email',$p_email);
+		$this->db->where('cliente_password',$p_password);
 		
-		$v_consulta = $this->db->get('usuarios');
+		$v_consulta = $this->db->get('clientes');
 		if($v_consulta->num_rows()==1){ //existe
-			$v_usuario = $v_consulta->row();
+			$v_cliente = $v_consulta->row();
 			$this->session->set_userdata(LOGGED,true);
-			$this->session->set_userdata('usuario',$v_usuario);
+			$this->session->set_userdata('cliente',$v_cliente);
 			$this->session->set_userdata(SESSION_LIVE,time());
 			return true;
 		}else{
@@ -47,12 +46,13 @@ class Seguridad_M extends CI_Model {
 	 * metodo que retorna permisos del usuario
 	 * @return array
 	 */
-	public function getPermisosUsuario()
+	//por el momento no tendremos en cuenta los permisos
+	/*public function getPermisosUsuario()
 	{
 		$this->db->select('*')->from('usuarios_perfiles up')->join('perfiles p', 'up.usuario_id = p.usuario_id');
 		$this->db->join('permisos per', 'per.perfil_id = p.perfil_id');
 		$this->db->where('usuario_id',$this->session->userdata('usuario')->usuario_id)->where('estado',E_ACTIVO);		
-	}
+	}*/
 	
 	/**
 	 * metodo para mantener con vida la sesion
@@ -66,15 +66,14 @@ class Seguridad_M extends CI_Model {
 	
 	/**
 	 * Retorna los datos de usuarios necesarios para el extjs
-	 * @param string $p_user_usuario
 	 */
-	public function getDatosUsuario(){
-		return $this->session->userdata('usuario');
+	public function getDatosCliente(){
+		return $this->session->userdata('cliente');
 	}
 	
 	/**
 	 * Comprueba que el usuario esta logueado y su tiempo de session no ha expirado aun.
-	 * @author Pablo
+	 * @author Juan Bauer
 	 * @return boolean
 	 */
 	public function logged(){
@@ -84,7 +83,6 @@ class Seguridad_M extends CI_Model {
 			//verificar que la ultima actividad con el sistema aun no haya caducado
 			if($this->config->item(SESSION_LIVE)< time() - $session_live  ){
 				$this->seguridad->logout();
-#				$v_sesion_activa = false;
 				return false;
 			}else{
 				return true;
@@ -96,4 +94,4 @@ class Seguridad_M extends CI_Model {
 	
 }
 
-/* End of file cliente.php */
+/* End of file seguridad.php */
